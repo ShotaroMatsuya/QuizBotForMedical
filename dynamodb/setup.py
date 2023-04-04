f4d90f691dfd4e60be2c0eba02ff87d55a71db13
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-04-04 11:14:40 +0900

    chore: dynamodb operation cli

--- dynamodb/setup.py
+++ dynamodb/setup.py
@@ -4,7 +4,7 @@ setup(
     name="cli-tools",
     version="1.0",
     py_modules=["csv_import"],
-    install_requires=["Click"],
+    install_requires=["boto3", "click", "tqdm"],
     entry_points={
         "console_scripts": ["import_csv=csv_import:cmd", "import_json=json_import:cmd"]
     },  # greetingsコマンド=greeterモジュールのgreetメソッド
